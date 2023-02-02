31271faa7b8150f73375255a789d22d57116c774
keduit <keduit@505-09>
2023-02-02 12:29:02 +0900

    test

---
+++ ttisme/src/main/java/com/keduit/ttisme/TtismeApplication.java
@@ -0,0 +1,13 @@
+package com.keduit.ttisme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtismeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtismeApplication.class, args);
+	}
+
+}
---
+++ ttisme/src/test/java/com/keduit/ttisme/TtismeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keduit.ttisme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtismeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
